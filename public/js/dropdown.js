95df74c8152658d2d1deaccb9b9a8dcecbf08291
Jose Albeiro Sánchez Ibañez <jsanchez@humboldt.org.co>
2025-03-28 16:30:54 -0500

    animacion boton

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
   const menuContainer = document.getElementById("menu-container");
   const menu = document.getElementById("dropdown-menu");
+  const button = document.getElementById("menu-button");
 
   if (menuContainer && menu) {
     menuContainer.addEventListener("mouseenter", () => {
       menu.classList.remove("hidden");
+      button?.classList.remove("animate-bounce");
     });
 
     menuContainer.addEventListener("mouseleave", () => {
       menu.classList.add("hidden");
+      button?.classList.add("animate-bounce");
     });
   }
 });
