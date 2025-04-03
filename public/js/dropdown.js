ee119bc897479e3d273d348f7a42b425e68ed033
Jose Albeiro Sánchez Ibañez <jsanchez@humboldt.org.co>
2025-04-03 12:35:54 -0500

    última actualización directorio

--- public/js/dropdown.js
+++ public/js/dropdown.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const menuContainer = document.getElementById("menu-container");
   const menu = document.getElementById("dropdown-menu");
   const button = document.getElementById("menu-button");
+  const textSpan = document.getElementById("menu-text");
 
   if (menuContainer && menu) {
     menuContainer.addEventListener("mouseenter", () => {
